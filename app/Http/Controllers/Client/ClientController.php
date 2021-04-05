589661437a99ac712c37577a266459489cce870e
zalviandyr <zukronalviandy@gmail.com>
2021-04-05 23:58:01 +0700

    add update profile client

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Kreait\Laravel\Firebase\Facades\Firebase;
 
 class ClientController extends Controller
 {
@@ -19,5 +22,27 @@ class ClientController extends Controller
         ];
 
         return $this->response($data);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $this->invalidValidResponse($request, [
+            'name' => 'required',
+        ]);
+
+        // update name in server
+        $userPhone = Auth::user()->phone_number;
+        $user = User::where('phone_number', $userPhone)->first();
+        $user->name = $request->name;
+        $user->save();
+
+        // update name in firebase
+        $auth = Firebase::auth();
+        $uid = $user->uid;
+        $auth->updateUser($uid, ['displayName' => $request->name]);
+
+        return $this->response([
+            'name' => $request->name
+        ], 'Success update profile');
     }
 }
